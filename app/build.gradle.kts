693e7211a04797270ecf8e3d742c570d1a404e31
8cee <aycee0@gmail.com>
2026-09-25 16:45:32 +0200

    Align Java and Kotlin JVM targets to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
 }
 android {
     namespace = "com.eightcee.marioparty1recomp"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     compileSdk = 35
     defaultConfig {
         applicationId = "com.eightcee.marioparty1recomp"
@@ -22,4 +27,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+}
+
+
+kotlin {
+    jvmToolchain(17)
 }
